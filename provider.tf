ed9ba753a4b9c55aa5b88d62e225df03f65a1993
bhavana <bhavanaamangarathi@gmail.com>
2023-08-05 13:29:48 +0530

    bhavana

--- provider.tf
+++ provider.tf
@@ -1,9 +1,18 @@
 provider "aws" {
   region = "us-east-2"
 }
-
 resource "aws_s3_bucket" "final" {
     bucket = "bhavana2001"
     
   
+}
+resource "aws_instance" "new" {
+
+    ami = "ami-024e6efaf93d85776"
+    instance_type = "t2.micro"
+    key_name = "khajasir"
+     
+     tags = {
+    Name = "Hiiia"
+  }
 }
